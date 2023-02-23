5d6dbf897db25712c6d93ff3c0b240eaeafcc36f
JoshuaWhittemore <josh_whittemore@mac.com>
2023-02-23 11:19:35 +0000

    Deploying to gh-pages from  @ 988672d25306ffa1d107a8bf7842331f23659e67 🚀

--- assets/index/ex1.jl
+++ assets/index/ex1.jl
@@ -1,6 +1,6 @@
 # This file was generated, do not modify it. # hide
 using LinearAlgebra, Random
-Random.seed!(135)
+Random.seed!(1)
 a, b = randn(50), randn(50)
 println(dot(a, b))
 println(sum(ai * bi for (ai, bi) ∈ zip(a, b)))
